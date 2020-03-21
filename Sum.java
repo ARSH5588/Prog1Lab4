e0717bf0d768c77eb26a1fa5a2b6f1f2040542a7
ARSH5588 <60395168+ARSH5588@users.noreply.github.com>
2020-03-21 18:47:47 -0400

    Done

--- Sum.java
+++ Sum.java
@@ -1,19 +1,21 @@
+//Importing the util import
 import java.util.*;
-
+///Class and main method
 public class Sum {
   public static void main(String[] args) {
-
+    //Variables
     int x = 10;
     int y = x + 1;
-
+    //What the program does
     System.out.println("\nThis program  will add all integers between 10 and 20 ");
-
+    //The initial value
     System.out.println("Initial Value = " + x);
-
+    //Start of the for loop
     for (int i = 1; i<11 ; i++) {
       x =  x + y;
       System.out.println("\t\t" + x);
       y++;
+      //End of the for loop
     }
 
   }
